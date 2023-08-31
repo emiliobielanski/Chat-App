6837a498b9d1c273237f16e0d8a899a3d309bd45
Emilio Bielanski <emilio.bielanski@outlook.com>
2023-08-31 14:25:26 +0200

    Register page done!

--- components/Register.js
+++ components/Register.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { Text, TextInput, View, StyleSheet, Button, Pressable } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 const Register = () => {
+    const navigation = useNavigation()
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const handleRegister = async () => {
+
+        try {
+
+            const credentials = {
+                username: username,
+                password: password
+            };
+
+            const response = await fetch("https://chat-api-with-auth.up.railway.app/auth/register",{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(credentials)
+            })
+
+            const data = await response.json();
+            if (data.status == "200") {
+                alert("User Successfully Registered! Redirecting...")
+                setTimeout(function(){
+                   navigation.navigate("Login")
+                }, 1000);
+            }   
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
   return (
     <View style={styles.container}>
-
-        <TextInput style={styles.username}/>
-        <TextInput  style={styles.password}/>
+        <FontAwesome name="user" size={180} color="black" />
+        <Text style={{marginTop: 30, alignSelf: "flex-start", marginLeft: "10%"}}>Desired Username:</Text>
+        <TextInput style={styles.username}  value={username}  onChangeText={setUsername}/>
+        <Text style={{alignSelf: "flex-start", marginLeft: "10%"}}>Password:</Text>
+        <TextInput  style={styles.password}  value={password} onChangeText={setPassword} secureTextEntry={true}/>
+        <Text style={{alignSelf: "flex-start", marginLeft: "10%"}}>Confirm Password:</Text>
+        <TextInput  style={styles.password} secureTextEntry={true} />
+        <Pressable style={styles.registerButton} onPress={handleRegister} >
+            <Text style={{fontSize: 20, fontWeight: "bold", color: "white"}}> Register User </Text>
+        </Pressable>
       
     </View>
   )
@@ -25,11 +69,25 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: "black",
         width: "80%",
-        margin: 20,
+        marginHorizontal: 20,
+        marginBottom: 16,
+        marginTop: 12,
     },
     password: {
         borderWidth: 2,
         borderColor: "black",
         width: "80%",
+        margin: 10,
+    },
+    registerButton: {
+        borderColor: "darkcyan",
+        borderWidth: 2,
+        backgroundColor: "cadetblue",
+        width: "50%",
+        height: "5%",
+        marginTop: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 20,
     }
 })
